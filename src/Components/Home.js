77d264a8623f20388d99f5e46d30a9830d247e31
n07Laksh <112006645+n07Laksh@users.noreply.github.com>
2023-08-03 23:13:49 +0530

    shift fetchuser api to profile

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { } from 'react'
 import Footer from './Footer';
 import Navbarfirst from './Navbarfirst';
 import Navbarsec from './Navbarsec';
@@ -7,41 +7,15 @@ import Profile from './Profile';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 
-function Home({ showAlert }) {
-    // const { showAlert } = props;
+function Home() {
     const location = useLocation()
-    useEffect(() => {
-        const getUser = async () => {
-            let data = await fetch("http://localhost:8000/api/auth/getuser", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "jwtoken": localStorage.getItem("user"),
-                }
-            });
-            data = await data.json();
-            console.log(data)
-            if (!data.error) {
-                localStorage.setItem("userDetail", JSON.stringify(data.user));
-                showAlert(`Welcome Again`, "success")
-            }
-            else {
-                showAlert(data.error, "danger")
-            }
-        }
-
-        if (!localStorage.getItem("userDetail")) {
-            getUser();
-        }
-        
-    },[])
 
     return (
         <div>
 
             {location.pathname === "/profile" ? (
                 <Routes>
-                    <Route exact path="/profile" element={<Profile />} />              
+                    <Route exact path="/profile" element={<Profile />} />
                 </Routes>
             ) : (
                 <>
